cfb075e183b0fb878d8692fadfd6753da645b654
Richard <Richard.Blaise@etu.univ-valenciennes.fr>
2018-12-06 23:21:04 +0100

    Revert "Déplacements"
    This reverts commit aa894e5427d34f5e28d4ecc7a9d6fee4153292e1.

--- arcade/src/states/Game.js
+++ arcade/src/states/Game.js
@@ -26,11 +26,7 @@ export default class extends Phaser.State {
     })
 
     this.game.add.existing(this.mushroom)
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.physics.arcade.gravity.y =200;
   }
-
-  
 
   render() {
     if (__DEV__) {
