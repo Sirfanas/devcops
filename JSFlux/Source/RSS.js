4ab90c0492eaf9a33eb2579b6d1a493bee5a00e3
rublood <ciesielski.Remi@gmail.com>
2018-12-07 00:36:50 +0100

    test rss

--- JSFlux/Source/RSS.js
+++ JSFlux/Source/RSS.js
@@ -1,12 +1,11 @@
-$(document).ready(
+/*$(document).ready(
     function(){
 
         $.ajax({
-            crossDomain: true,
-            type: "PUT",
-            contentType : "application/x-www-form-urlencoded",
-            url: "http://www.48info.fr/rss.php",
-            data : {categorie:1},
+            crossdomain: false,
+            type: "GET",
+            url: "https://www.francetvinfo.fr/france.rss",
+            dataType: "xml",
             success: function(xml) {
                 $(xml).find('channel').each(function(){
                     console.log(xml);
@@ -15,7 +14,7 @@ $(document).ready(
         }
       );
     }
-);
+);*/
  /*var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -24,3 +23,30 @@ $(document).ready(
   };
   xhttp.open("GET","https://www.lesechos.fr/rss/rss_une_titres.xml", true);
   xhttp.send();*/
+$(document).ready(function() {
+    //feed to parse
+    var feed = "https://www.francetvinfo.fr/france.rss";
+    
+    $.ajax(feed, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        crossdomain: true,
+        headers: {  'Access-Control-Allow-Origin': '*' },
+        dataType:"xml",
+        success:function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+    
+
+        }   
+    });
+    
+});
