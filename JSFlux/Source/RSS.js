ee07bfa3248962a42e40511f27af6451124f9d8f
= <delphinrukundo10@gmail.com>
2018-12-07 06:00:19 +0100

    Traffic and Weather OK

--- JSFlux/Source/RSS.js
+++ JSFlux/Source/RSS.js
@@ -1,6 +1,5 @@
 $(document).ready(
     function(){
-
         $.ajax({
             type: "GET",
             url: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lesechos.fr%2Frss%2Frss_une_titres.xml",
--- JSFlux/Weather/weather.js
+++ JSFlux/Weather/weather.js
@@ -10,7 +10,7 @@ function getWeather() {
 /**
  * Treat the complete json object returned from the API call.
  */
-function parse(json) { 
+function parse(json) {
     var weather = json.weather;
     var main = json.main;
     var weath = weather[0].main;
@@ -21,18 +21,18 @@ function parse(json) {
 }
 
 /**
- * Translate the current weather in french and add a little icon next to the data. 
+ * Translate the current weather in french and add a little icon next to the data.
  */
 function treatWeather(weather) {
     var img = "";
     switch(weather) {
         case "Drizzle": weather = "Bruine"; img = "<i class=\"fas fa-cloud-rain\"></i>"; break;
-        case "Mist": weather = "Brouillard"; img = "<i class=\"fas fa-cloud\"></i>"; break;
-        case "Snow": weather = "Neige"; img = "<i class=\"fas fa-snowflake\"></i>"; break;
-        case "Rain": weather = "Pluie"; img = "<i class=\"fas fa-cloud-rain\"></i>"; break;
-        case "Clouds": weather = "Nuage"; img = "<i class=\"fas fa-cloud\"></i>"; break;
-        case "Haze": weather = "Brume"; img = "<i class=\"fas fa-cloud\"></i>"; break;
-        case "Clear": weather = "Dégagé"; img = "<i class=\"fas fa-sun\"></i>"; break;
+        case "Mist": weather = "Brouillard"; img = "<i class=\"fa fa-cloud\"></i>"; break;
+        case "Snow": weather = "Neige"; img = "<i class=\"fa fa-snowflake\"></i>"; break;
+        case "Rain": weather = "Pluie"; img = "<i class=\"fa fa-cloud-rain\"></i>"; break;
+        case "Clouds": weather = "Nuage"; img = "<i class=\"fa fa-cloud\"></i>"; break;
+        case "Haze": weather = "Brume"; img = "<i class=\"fa fa-cloud\"></i>"; break;
+        case "Clear": weather = "Dégagé"; img = "<i class=\"fa fa-sun\"></i>"; break;
         default: break;
     }
     $('#weather-inf-wea').html(img);
