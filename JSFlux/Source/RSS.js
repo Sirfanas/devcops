1248069b18f42c2265222ca440f196962a717d7b
= <delphinrukundo10@gmail.com>
2018-12-07 06:33:19 +0100

    RSS OK

--- JSFlux/Source/RSS.js
+++ JSFlux/Source/RSS.js
@@ -10,7 +10,7 @@ $(document).ready(
                root.append('<div class="title"><strong>'+data.feed.title+'</strong></div>\n<div class="messages">');
                $.each(data.items,function(index,element){
                 var date = new Date(element.pubDate);
-                    $('.messages').append('<a href="'+element.link+'" class="message d-flex align-items-center"><div class="profile"><img src="'+element.enclosure.link+'" alt="..." class="img-fluid"></div><div class="content"> <strong class="d-block">'+element.title+'</strong><span class="d-block">'+element.description+'</span><small class="date d-block">'+ date.getDate() + '/' + (date.getMonth() + 1) + '/' +  date.getFullYear()+'</small></div></a>')
+                    $('.messages').append('<a href="'+element.link+'" class="message d-flex align-items-center"><div class="profile"></div><div class="content"> <strong class="d-block">'+element.title+'</strong><span class="d-block">'+element.description+'</span><small class="date d-block">'+ date.getDate() + '/' + (date.getMonth() + 1) + '/' +  date.getFullYear()+'</small></div></a>')
                })
 
             }
