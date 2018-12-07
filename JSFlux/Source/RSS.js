41231796b8a02ceec524a6c0d5e331344cf618a3
rublood <ciesielski.Remi@gmail.com>
2018-12-07 01:36:44 +0100

    fix cross domain

--- JSFlux/Source/RSS.js
+++ JSFlux/Source/RSS.js
@@ -1,52 +1,14 @@
-/*$(document).ready(
+$(document).ready(
     function(){
 
         $.ajax({
-            crossdomain: false,
             type: "GET",
-            url: "https://www.francetvinfo.fr/france.rss",
-            dataType: "xml",
-            success: function(xml) {
-                $(xml).find('channel').each(function(){
-                    console.log(xml);
-                })
+            url: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lesechos.fr%2Frss%2Frss_une_titres.xml",
+            dataType: "jsonp",
+            success: function(data) {
+               console.log(data);
             }
         }
       );
     }
-);*/
- /*var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xhttp.open("GET","https://www.lesechos.fr/rss/rss_une_titres.xml", true);
-  xhttp.send();*/
-$(document).ready(function() {
-    //feed to parse
-    var feed = "https://www.francetvinfo.fr/france.rss";
-    
-    $.ajax(feed, {
-        accepts:{
-            xml:"application/rss+xml"
-        },
-        crossdomain: true,
-        headers: {  'Access-Control-Allow-Origin': '*' },
-        dataType:"xml",
-        success:function(data) {
-            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
-
-            $(data).find("item").each(function () { // or "item" or whatever suits your feed
-                var el = $(this);
-                console.log("------------------------");
-                console.log("title      : " + el.find("title").text());
-                console.log("link       : " + el.find("link").text());
-                console.log("description: " + el.find("description").text());
-            });
-    
-
-        }   
-    });
-    
-});
+);
